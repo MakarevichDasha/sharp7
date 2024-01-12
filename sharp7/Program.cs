a4b01a5dade829662b9a12a86dedbc4a4e3c1715
Дашка <@LAPTOP-RUOGSO7S>
2024-01-12 13:32:46 +0300

    Добавьте файлы проекта.

---
+++ sharp7/Program.cs
@@ -0,0 +1,29 @@
+﻿using System.Text.RegularExpressions;
+
+// Открывается файл "input.txt" для чтения
+StreamReader input = new StreamReader("input.txt");
+
+// Создается объект StreamWriter для записи в файл "output.txt"
+StreamWriter output = new StreamWriter("output.txt");
+
+// Читается весь текст из файла и сохраняется в строке s
+string s = input.ReadToEnd();
+
+// Получается последняя цифра строки и преобразуется в целое число
+int k = Convert.ToInt32(s.Substring(s.Length - 1, 1));
+
+// Удаляется последний символ из строки s
+s = s.Substring(0, s.Length - 1);
+
+// Находятся все слова длиной не менее k символов в строке s
+MatchCollection m = new Regex(@"\b\w{" + k + @",}\b").Matches(s);
+
+// Если найдены слова, выводится их количество; иначе выводится -1 (при помощи иф элс)
+if (m.Count > 0)
+    output.WriteLine(m.Count);
+else
+    output.WriteLine(-1);
+
+// Закрываются потоки чтения и записи
+input.Close();
+output.Close();
